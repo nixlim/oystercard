613ed59c1c6882f8b36051e606ab9942f771599b
Asia Antczak <aasiaantczak@gmail.com>
2018-04-25 18:29:19 +0100

    add finish method

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,9 +2,13 @@ class Journey
   MINIMUM_FARE = 1
   PENALTY_FARE = 6
 
-  def start_journey(entry_station, balance)
+  def initialize(entry_station, balance)
     @entry_station = entry_station
     @balance = balance
+  end
+
+  def finish_journey(exit_station, balance)
+    @exit_station = exit_station
   end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -4,21 +4,23 @@ describe Journey do
   let(:card) { instance_double Oystercard, balance: 2 }
   let(:entry_station) { instance_double Station, name: "Liverpool Street", zone: 1 }
   let(:exit_station) { instance_double Station, name: "Paddington Station", zone: 2 }
+  let(:journey) {described_class.new(entry_station, card.balance)}
 
 
-
-  describe '#start_journey' do
-    it { is_expected.to respond_to(:start_journey).with(2).arguments }
-
+  describe '#initialize' do
     it 'stores an entry_station' do
-      subject.start_journey(entry_station, card.balance)
-      expect(subject.instance_variable_get(:@entry_station)).to eq(entry_station)
+      expect(journey.instance_variable_get(:@entry_station)).to eq(entry_station)
     end
 
     it 'stores a balance' do
-      subject.start_journey(entry_station, card.balance)
-      expect(subject.instance_variable_get(:@balance)).to eq(2)
+      expect(journey.instance_variable_get(:@balance)).to eq(2)
     end
+  end
 
+  describe '#finish journey' do
+    it 'stores an exit station' do
+      journey.finish_journey(exit_station, card.balance)
+      expect(journey.instance_variable_get(:@exit_station)).to eq(exit_station)
+    end
   end
 end
