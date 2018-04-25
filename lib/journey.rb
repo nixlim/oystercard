d1ce6a7d37231ad183b25dfd48cad0e7f463c973
Igor Ryabchuk <riabtchuk@gmail.com>
2018-04-25 17:22:18 +0100

    add entry_station initialisation variable

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,3 +1,10 @@
 class Journey
+  MINIMUM_FARE = 1
+  PENALTY_FARE = 6
+
+  def start_journey(entry_station, balance)
+    @entry_station = entry_station
+
+  end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,8 +1,18 @@
 require 'journey'
 
 describe Journey do
+  let(:card) { instance_double Oystercard, balance: 2 }
+  let(:entry_station) { instance_double Station, name: "Liverpool Street", zone: 1 }
+  let(:exit_station) { instance_double Station, name: "Paddington Station", zone: 2 }
 
 
 
+  describe '#start_journey' do
+    it { is_expected.to respond_to(:start_journey).with(2).arguments }
 
+    it 'stores an entry_station' do
+      subject.start_journey(entry_station, card.balance)
+      expect(subject.instance_variable_get(:@entry_station)).to eq(entry_station)
+    end
+  end
 end
