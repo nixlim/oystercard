99e9a7661daa887835ed1c7e5cfe7402cda2410a
Charly Mannion <charlottemannion100@gmail.com>
2018-04-27 14:56:15 +0100

    journey calculates fare

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,17 +2,22 @@ class Journey
   MINIMUM_FARE = 1
   PENALTY_FARE = 6
 
-  def initialize
+  attr_reader :entry_station, :exit_station
 
+  def start_journey(entry_station)
+    @entry_station = entry_station
   end
 
-  def finish_journey(exit_station, balance)
+  def end_journey(exit_station)
     @exit_station = exit_station
   end
 
-
   def complete?
+    exit_station.nil? || entry_station.nil? ? false : true
+  end
 
+  def fare
+    complete? ? MINIMUM_FARE : PENALTY_FARE
   end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,26 +1,49 @@
 require 'journey'
 
 describe Journey do
-  let(:card) { instance_double Oystercard, balance: 2 }
   let(:entry_station) { instance_double Station, name: "Liverpool Street", zone: 1 }
   let(:exit_station) { instance_double Station, name: "Paddington Station", zone: 2 }
-  let(:journey) {described_class.new(entry_station, card.balance)}
+  let(:journey) {described_class.new}
 
-
-  describe '#initialize' do
-    it 'stores an entry_station' do
-      expect(journey.instance_variable_get(:@entry_station)).to eq(entry_station)
-    end
-
-    it 'stores a balance' do
-      expect(journey.instance_variable_get(:@balance)).to eq(2)
+  describe '#start_journey' do
+    it 'should record the entry station' do
+      journey.start_journey(entry_station)
+      expect(journey.entry_station).to eq entry_station
     end
   end
 
-  describe '#finish journey' do
-    it 'stores an exit station' do
-      journey.finish_journey(exit_station, card.balance)
-      expect(journey.instance_variable_get(:@exit_station)).to eq(exit_station)
+  describe '#end_journey' do
+    it 'should record the exit station' do
+      journey.end_journey(exit_station)
+      expect(journey.exit_station).to eq exit_station
     end
   end
+
+  describe '#complete?' do
+    it 'should return true if journey was started and ended' do
+      journey.start_journey(entry_station)
+      journey.end_journey(exit_station)
+      expect(journey.complete?).to be true
+    end
+    it 'should return false if journey was started but not ended' do
+      journey.start_journey(entry_station)
+      expect(journey.complete?).to be false
+    end
+    it 'should return false if journey was ended but not started' do
+      journey.end_journey(exit_station)
+      expect(journey.complete?).to be false
+    end
+  end
+
+  describe '#fare' do
+    it 'should charge the penalty fare if journey not complete' do
+      allow(journey).to receive(:complete?) { false }
+      expect(journey.fare).to eq Journey::PENALTY_FARE
+    end
+    it 'should charge the minimum fare if journey complete' do
+      allow(journey).to receive(:complete?) { true }
+      expect(journey.fare).to eq Journey::MINIMUM_FARE
+    end
+  end
+
 end
