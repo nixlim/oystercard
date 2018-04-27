3af4060b027e155d5e4ae26488b93c8a3f24424e
Igor Ryabchuk <riabtchuk@gmail.com>
2018-04-27 09:47:09 +0100

    refactoring

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -20,72 +20,73 @@ describe Oystercard do
   end
 
 
-describe '#top_up' do
-  it 'should return correct balance when topped up' do
-    expect { oystercard.top_up(10) }.to change { oystercard.balance }.by(10)
-  end
+  describe '#top_up' do
+    it 'should return correct balance when topped up' do
+      expect { oystercard.top_up(10) }.to change { oystercard.balance }.by(10)
+    end
 
   it 'should raise error if total exceeds max limit' do
     expect { oystercard.top_up(100) }.to raise_error("Cannot top up as balance exceeds maximum limit.")
   end
 end
 
-describe '#touch_in' do
-  it 'should check whether the last journey is complete' do
-    subject.instance_variable_set(:@balance, 20)
-    subject.instance_variable_set(:@journey_class, journey_class)
-    subject.touch_in(entry_station)
-    expect(journey).to have_received(:fare)
-  end
-  it 'should raise an error message if balance on #touch_in is less than £1' do
-    subject.instance_variable_set(:@journey_class, journey_class)
-    expect { subject.touch_in(entry_station) }.to raise_error 'Insufficient funds for a journey'
-  end
-
-  context 'changes caused by touch_in' do
-    before(:each) do
-      subject.instance_variable_set(:@balance, 20)
-    end
-    it 'should send #complete? to journey on touch_in' do
-      subject.instance_variable_set(:@journey_class, journey_class)
-      allow(journey).to receive(:complete?).and_return(false)
-      subject.touch_in(entry_station)
-      expect(subject).to be_in_journey
-    end
-    it 'should remember the entry station' do
-      subject.instance_variable_set(:@journey_class, journey_class)
-      subject.touch_in(entry_station)
-      variable = subject.instance_variable_get(:@journey)[:entry_station]
-      expect(variable).to eq entry_station
-    end
-  end
-
-end
-
-describe '#touch_out' do
-
-  it 'should deduct minimum fare upon #touch_out' do
-    subject.instance_variable_set(:@balance, 20)
-    subject.instance_variable_set(:@journey_class, journey_class)
-    subject.touch_in(entry_station)
-    expect { subject.touch_out(exit_station) }.to change { subject.balance }.by(-1)
-  end
-  context 'touch out affects in_journey? method' do
-    before(:each) do
+  describe '#touch_in' do
+    it 'should check whether the last journey is complete' do
       subject.instance_variable_set(:@balance, 20)
       subject.instance_variable_set(:@journey_class, journey_class)
       subject.touch_in(entry_station)
+      expect(journey).to have_received(:fare)
     end
-    it 'should store exit_station on touch_out' do
-      subject.touch_out(exit_station)
-      expect(subject.list_of_journeys.last[:exit_station]).to eq exit_station
+    it 'should raise an error message if balance on #touch_in is less than £1' do
+      subject.instance_variable_set(:@journey_class, journey_class)
+      expect {
+        subject.touch_in(entry_station)
+      }.to raise_error 'Insufficient funds for a journey'
     end
-    it 'should change in_journey? on touch_out' do
-      subject.touch_out(exit_station)
-      expect(subject).not_to be_in_journey
+    context 'changes caused by touch_in' do
+      before(:each) do
+        subject.instance_variable_set(:@balance, 20)
+      end
+      it 'should send #complete? to journey on touch_in' do
+        subject.instance_variable_set(:@journey_class, journey_class)
+        allow(journey).to receive(:complete?).and_return(false)
+        subject.touch_in(entry_station)
+        expect(subject).to be_in_journey
+      end
+      it 'should remember the entry station' do
+        subject.instance_variable_set(:@journey_class, journey_class)
+        subject.touch_in(entry_station)
+        variable = subject.instance_variable_get(:@journey)[:entry_station]
+        expect(variable).to eq entry_station
+      end
     end
   end
-end
+
+  describe '#touch_out' do
+    it 'should deduct minimum fare upon #touch_out' do
+      subject.instance_variable_set(:@balance, 20)
+      subject.instance_variable_set(:@journey_class, journey_class)
+      subject.touch_in(entry_station)
+      expect { subject.touch_out(exit_station) }.to change {
+        subject.balance
+      }.by(-1)
+    end
+    context 'touch out affects in_journey? method' do
+      before(:each) do
+        subject.instance_variable_set(:@balance, 20)
+        subject.instance_variable_set(:@journey_class, journey_class)
+        subject.touch_in(entry_station)
+      end
+      it 'should store exit_station on touch_out' do
+        subject.touch_out(exit_station)
+        expect(subject.list_of_journeys.last[:exit_station]).to eq exit_station
+      end
+      it 'should change in_journey? on touch_out' do
+        subject.touch_out(exit_station)
+        expect(subject).not_to be_in_journey
+      end
+    end
+  end
 
   context 'touching in and out' do
     before(:each) do
@@ -99,5 +100,4 @@ end
       expect(subject.list_of_journeys.length).to eq 1
     end
   end
-
 end
